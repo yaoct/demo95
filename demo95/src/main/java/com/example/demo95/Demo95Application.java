28b5286695c947ff0180a9bda84c0a11bf0b6bac
yaoct <420595421@qq.com>
2020-09-06 01:52:35 +0800

    2222

--- demo95/src/main/java/com/example/demo95/Demo95Application.java
+++ demo95/src/main/java/com/example/demo95/Demo95Application.java
@@ -7,9 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo95Application {
 
     public static void main(String[] args) {
-        for(String arg:args){
-            System.out.println(arg);
-        }
+//        for(String arg:args){
+//            System.out.println(arg);
+//        }
         SpringApplication.run(Demo95Application.class, args);
     }
 
